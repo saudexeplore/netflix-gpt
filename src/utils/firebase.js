96be84e9c12fb643d270c6663925fc336b288c82
Mohd Aadil Rana <aadil.rana@tern-group.com>
2024-07-25 23:14:29 +0530

    new deploy firbase issue fixed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtGWM2G2vt4_X__V9PO79QyVoQnRb0CPc",
-  authDomain: "netflixgpt-91cd7.firebaseapp.com",
-  projectId: "netflixgpt-91cd7",
-  storageBucket: "netflixgpt-91cd7.appspot.com",
-  messagingSenderId: "458407883952",
-  appId: "1:458407883952:web:7f8459302f501feeb6b775",
-  measurementId: "G-LL9ZDV4RY5"
+  apiKey: "AIzaSyD7Th9M-IaXU84bo2RJcFFl7Ev65uqxaPs",
+  authDomain: "netflixgpt2-4d48c.firebaseapp.com",
+  projectId: "netflixgpt2-4d48c",
+  storageBucket: "netflixgpt2-4d48c.appspot.com",
+  messagingSenderId: "959362674656",
+  appId: "1:959362674656:web:3691d06087cf26c7990585",
+  measurementId: "G-3N4K51KN8Z"
 };
 
 // Initialize Firebase
